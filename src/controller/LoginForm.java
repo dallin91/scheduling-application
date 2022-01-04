2606a43b37de7127142ec74d3aa7effa40731fca
Dallin Reeves <dallinreeves@gmail.com>
2022-01-03 18:14:19 -0600

    added scheduling page and scene change

--- src/controller/LoginForm.java
+++ src/controller/LoginForm.java
@@ -2,7 +2,10 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -48,6 +51,12 @@ public class LoginForm implements Initializable {
         if (user.equals("test") && password.equals("test")) {
             //transition to new scene
             System.out.println("Yippee it worked!");
+            Parent root = FXMLLoader.load(getClass().getResource("/view/SchedulingPage.fxml"));
+            Stage stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setTitle("Scheduling Page");
+            stage.setScene(scene);
+            stage.show();
         }
         else {
             //add an error message
