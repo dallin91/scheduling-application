adb2aa62cf8237e980470b101b6e45399f0ad69b
Dallin Reeves <dallinreeves@gmail.com>
2022-01-18 17:02:11 -0600

    added labels and text fields

--- src/controller/AddCustomer.java
+++ src/controller/AddCustomer.java
@@ -1,4 +1,24 @@
 package controller;
 
-public class AddCustomer {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddCustomer implements Initializable {
+    @FXML
+    private TextField custName;
+    @FXML
+    private TextField addressField;
+    @FXML
+    private TextField zipCode;
+    @FXML
+    private TextField phoneNumber;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+    }
 }
