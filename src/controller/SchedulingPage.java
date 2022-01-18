b7cda23903f222ef881b7501248d316b1be7a9bd
Dallin Reeves <dallinreeves@gmail.com>
2022-01-18 17:12:05 -0600

    scene change to add customer page

--- src/controller/SchedulingPage.java
+++ src/controller/SchedulingPage.java
@@ -1,4 +1,33 @@
 package controller;
 
-public class SchedulingPage {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SchedulingPage implements Initializable {
+    @FXML
+    private Button addCustBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+    }
+
+    public void toAddCustomer(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/AddCustomer.fxml"));
+        Stage stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setTitle("Add Customer");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
