adf076eb09318cb212b0bae5165b4d1db41f7803
Dallin Reeves <dallinreeves@gmail.com>
2022-02-10 19:27:17 -0600

    initial commit

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,4 +1,109 @@
 package model;
 
 public class Appointment {
+
+    private int appointmentId;
+    private String title;
+    private String description;
+    private String location;
+    private String contactName;
+    private String type;
+    private String startTime;
+    private String endTime;
+    private int customerId;
+    private int userId;
+
+    public Appointment(int appointmentId, String title, String description, String location, String contactName,
+                       String type, String startTime, String endTime, int customerId, int userId) {
+        this.appointmentId = appointmentId;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.contactName = contactName;
+        this.type = type;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.customerId = customerId;
+        this.userId = userId;
+    }
+
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,4 +1,91 @@
 package model;
 
 public class Customer {
+
+    private int id;
+
+    private String name;
+
+    private String address;
+
+    private String zipCode;
+
+    private String phoneNumber;
+
+    private String country;
+
+    private String customerArea;
+
+
+    //constructor
+    public Customer(int id, String name, String address, String zipCode, String phoneNumber, String country,
+                    String customerArea) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.country = country;
+        this.customerArea = customerArea;
+    }
+
+    //get id
+    public int getId() {
+        return id;
+    }
+
+    //set id
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    //get name
+    public String name() {
+        return name;
+    }
+
+    //set name
+    public void name(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCustomerArea() {
+        return customerArea;
+    }
+
+    public void setCustomerArea(String customerArea) {
+        this.customerArea = customerArea;
+    }
 }
