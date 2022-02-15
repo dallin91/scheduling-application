627e5f7dac4dbc70322363af7fcc481dbf0a217c
Dallin Reeves <dallinreeves@gmail.com>
2022-02-15 17:36:18 -0600

    commented out dbconnection in main until ready

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import Database.DBConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,6 +17,9 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        //DBConnection.startConnection();
+
         launch(args);
+        //DBConnection.closeConnection();
     }
 }
