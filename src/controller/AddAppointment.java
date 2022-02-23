d8f3ec57ab1d6b8982a05a1ac9ee2b45e4589490
Dallin Reeves <dallinreeves@gmail.com>
2022-02-23 17:16:06 -0600

    added cancel button functionality

--- src/controller/AddAppointment.java
+++ src/controller/AddAppointment.java
@@ -1,4 +1,22 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AddAppointment {
+
+    public void toSchedulingCancel(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/SchedulingPage.fxml"));
+        Stage stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setTitle("Scheduling Page");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
