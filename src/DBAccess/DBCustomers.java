67be0503b58dbe4e71f18588b07d08c1e9e2e480
Dallin <dallinreeves@gmail.com>
2022-03-01 18:44:50 -0500

    set up DBCustomers

--- src/DBAccess/DBCustomers.java
+++ src/DBAccess/DBCustomers.java
@@ -10,21 +10,32 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DBCustomers {
-    public static ObservableList<Customer> getCustomerObservableList() throws SQLException {
-        ObservableList<Customer> customerObservableList = FXCollections.observableArrayList();
-        String sql = "SELECT * from customers";
-        PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
-        ResultSet rs = ps.executeQuery();
-        while(rs.next()) {
-            int id = rs.getInt("Customer_ID");
-            String name = rs.getString("Customer_Name");
-            String address = rs.getString("Address");
-            String zipCode = rs.getString("Postal_Code");
-            String phoneNumber = rs.getString("Phone");
-            int divisionID = rs.getInt("Division_ID");
-            Customer customer = new Customer(id, name, address, zipCode, phoneNumber, divisionID);
-            customerObservableList.add(customer);
+
+    public static ObservableList<Customer> getAllCustomers() {
+        ObservableList<Customer> customersList = FXCollections.observableArrayList();
+
+        try {
+            String sql = "SELECT * from customers";
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()) {
+                int customerID = rs.getInt("Customer_ID");
+                String customerName = rs.getString("Customer_Name");
+                String customerAddress = rs.getString("Address");
+                String zipCode = rs.getString("Postal_Code");
+                String phoneNumber = rs.getString("Phone");
+                int divisionID = rs.getInt("Division_ID");
+                Customer customer = new Customer(customerID, customerName, customerAddress, zipCode, phoneNumber,
+                        divisionID);
+                customersList.add(customer);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return customerObservableList;
+
+
+        return customersList;
     }
 }
