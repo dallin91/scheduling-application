8d15620a8df446112ce2ec002325576ca5a6e827
Dallin <dallinreeves@gmail.com>
2022-03-21 18:18:25 -0400

    updating field FXIDs

--- src/controller/AddAppointment.java
+++ src/controller/AddAppointment.java
@@ -1,11 +1,15 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +17,31 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddAppointment implements Initializable {
+
+    @FXML
+    private TextField apptTitle;
+    @FXML
+    private TextField apptDescription;
+    @FXML
+    private TextField apptLocation;
+    @FXML
+    private TextField apptType;
+    @FXML
+    private DatePicker apptStartDate;
+    @FXML
+    private DatePicker apptEndDate;
+    @FXML
+    private ComboBox<String> custID;
+    @FXML
+    private ComboBox<String> userID;
+    @FXML
+    private TextField apptID;
+    @FXML
+    private ComboBox<String> contactID;
+    @FXML
+    private ComboBox<String> apptStartTime;
+    @FXML
+    private ComboBox<String> apptEndTime;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
