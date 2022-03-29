548614261e9af0870149e8c7272e6ea4ad5f6486
Dallin <dallinreeves@gmail.com>
2022-03-28 22:01:09 -0400

    set up alert for appts within 15 minutes of login

--- src/controller/LoginForm.java
+++ src/controller/LoginForm.java
@@ -1,5 +1,6 @@
 package controller;
 
+import DBAccess.DBAppointments;
 import DBAccess.DBUsers;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -13,11 +14,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.Appointment;
 import model.User;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -43,6 +46,7 @@ public class LoginForm implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
         zoneAndLanguage();
+        checkUpcomingAppointments();
     }
 
     public void handleCancelButtonAction(ActionEvent event) {
@@ -101,5 +105,30 @@ public class LoginForm implements Initializable {
             cancelBtn.setText(rb.getString("Cancel").replaceAll(",", " "));
         }
         */
+    }
+
+    public void checkUpcomingAppointments() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime soon = now.plusMinutes(15);
+        ObservableList<Appointment> appointmentObservableList = DBAppointments.getAllAppointments();
+        ObservableList<User> userObservableList = DBUsers.getAllUsers();
+        String userName = idField.getText();
+        int idToCheck = 0;
+        for (User u : userObservableList) {
+            if (u.getUserName().equals(userName)) {
+                idToCheck = u.getUserID();
+                break;
+            }
+        }
+
+        for (Appointment a : appointmentObservableList) {
+            if (a.getUserId() == idToCheck && a.getStartTime().isBefore(soon)) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Appointment Soon");
+                alert.setContentText("Appointment scheduled within 15 minutes");
+                alert.showAndWait();
+                return;
+            }
+        }
     }
 }
