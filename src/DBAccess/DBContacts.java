328e490fc28e10a314d84d2df12b00cbc54342c6
Dallin <dallinreeves@gmail.com>
2022-03-30 11:08:48 -0400

    added javadoc comments

--- src/DBAccess/DBContacts.java
+++ src/DBAccess/DBContacts.java
@@ -10,8 +10,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * This class serves as the way to access the contacts table in the database
+ *
+ * @author Dallin Reeves
+ * */
 public class DBContacts {
 
+    /**
+     * Returns all contacts
+     * */
     public static ObservableList<Contact> getAllContacts() {
         ObservableList<Contact> contactsList = FXCollections.observableArrayList();
 
