3974c7df75631e79f8c73ce479c0764a8a022e36
Dallin <dallinreeves@gmail.com>
2022-03-30 10:51:20 -0400

    added javadoc comments

--- src/DBAccess/DBCountries.java
+++ src/DBAccess/DBCountries.java
@@ -9,8 +9,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * This class serves as the way to access the countries table in the database
+ *
+ * @author Dallin Reeves
+ * */
 public class DBCountries {
 
+    /**
+     * Returns all countries
+     * */
     public static ObservableList<Country> getAllCountries() {
         ObservableList<Country> countryList = FXCollections.observableArrayList();
 
