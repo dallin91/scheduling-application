e37c0111cae41665aa806c373d5de373e2ceb737
Dallin <dallinreeves@gmail.com>
2022-03-30 10:43:00 -0400

    added javadoc comments

--- src/Database/DBUtility.java
+++ src/Database/DBUtility.java
@@ -4,15 +4,29 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+/**
+ * This class allows the user to interact with the database
+ *
+ * @author Dallin Reeves
+ * */
 public class DBUtility {
 
     private static PreparedStatement preparedStatement;
 
 
+    /**
+     * Sets prepared statement
+     *
+     * @param conn
+     * @param sqlStatement
+     * */
     public static void setPreparedStatement(Connection conn, String sqlStatement) throws SQLException {
         preparedStatement = conn.prepareStatement(sqlStatement);
     }
 
+    /**
+     * Returns prepared statement
+     * */
     public static PreparedStatement getPreparedStatement() {
         return preparedStatement;
     }
